db0c241e6a94aabc19f72f71a455717533dd985f
csujeet <lightingdesign581@gmail.com>
2025-07-08 20:49:19 +0545

    Update layout.tsx
    placed google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1112506196784687"
+         crossorigin="anonymous"></script>
       </head>
       <body className={`${inter.className} antialiased`} suppressHydrationWarning>
         {children}
